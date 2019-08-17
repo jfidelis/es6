8ed994917226b16c864adc550b4f6633df84ef56
Jeferson IP Fidelis <jefersonfidelis@terra.com.br>
2019-08-17 16:07:54 -0300

    Webpack Start my jobs

---
+++ app/index.js
@@ -0,0 +1,7 @@
+import Message from './model/message.model';
+// import ImageMessage from './model/image-message.model';
+
+const m = new Message();
+// const m = new ImageMessage();
+console.log('not empty');
+console.dir(m);
