6e123bbaef061f53029926841c409ad446760760
Jeferson IP Fidelis <jefersonfidelis@terra.com.br>
2019-08-18 09:38:01 -0300

    Hot Module Replacement job

--- app/index.js
+++ app/index.js
@@ -1,4 +1,17 @@
 import Message from './model/message.model';
+/* eslint no-console: 0 */
+/* eslint no-undef: 0 */
+/* eslint indent: 0 */
 
 console.log('Index started');
 console.dir(new Message());
+
+document.getElementById('send').onclick = () => {
+    const m = new Message(document.getElementById('message').value);
+    document.getElementById('messages').innerHTML +=
+    `<li>${m.text} - ${m.created} </li>`;
+};
+
+if (module && module.hot) {
+    module.hot.accept();
+}
